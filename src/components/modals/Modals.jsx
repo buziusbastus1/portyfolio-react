74871949f993cd0f619563629f5459285336fa2d
buziusbastus1 <kubaotreba121@wp.pl>
2023-04-23 00:44:42 +0200

    modal

--- src/components/modals/Modals.jsx
+++ src/components/modals/Modals.jsx
@@ -3,10 +3,13 @@ import './modals.css'
 const Modal = ({ title, content, onClose, img }) => {
   return (
     <div className="modal">
-      <div className="modal__content">
-        <button className="modal__close" onClick={onClose}>&times;</button>
+        <div className='modhead'>
         <h3>{title}</h3>
+        <button className="modal__close" onClick={onClose}>&times;</button>
+        </div>
+      <div className="modal__content">
         <p>{content}</p>
+
            <div className="modal__image-wrapper">
           <img src={img} alt={title} />
           <img src={img} alt={title} />
