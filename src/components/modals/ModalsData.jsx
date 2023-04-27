d6b7c3d6f062c2101c6be9c890d4efae2b9b4025
buziusbastus1 <kubaotreba121@wp.pl>
2023-04-27 17:12:08 +0200

    modal

--- src/components/modals/ModalsData.jsx
+++ src/components/modals/ModalsData.jsx
@@ -18,9 +18,9 @@ export const data = [
     title: 'Grafika',
     github: 'https://github.com/buziusbastus1/grafika',
     demo: 'https://buziusbastus1.github.io/grafika/',
-    modalContent: 'Modal conten for chart templatesModal conten for chart templatesModal conten for chart templatesModal conten for chart templatesModal conten for chart templatesModal conten for chart templates',
+    modalContent: '<p>This was my first website uploaded to a github. The course posted on a freecodecamp.org YouTube channel named <a href="https://youtu.be/3PHXvlpOkf4conten" target="_blank" rel="noreferrer">Build 15 JavaScript Projects - Vanilla JavaScript Course</a> inspired my to build my own website.<p>It contains buttons appearing on hover or click on touch-screen devices. Two buttons open up text container in the same place which you can close. The other buttons open up the modals with filtering of images and ability to click on them in order to zoom and swipe them with the <a href="https://photoswipe.com/" target="_blank" rel="noreferrer">PhotoSwipe</a>.</p>',
     modalImage: graf,
-    technologies: '<li>Vanilla JS</li><li>SCSS</li><li>RWD</li>'
+    technologies: '<li>Vanilla JS</li><li>HTML</li><li>SCSS</li><li>RWD</li><li>PhotoSwipe</li>'
   },
   {
     id: 2,
@@ -28,7 +28,7 @@ export const data = [
     title: 'Guitar Display',
     github: 'https://github.com/buziusbastus1/guitar-display',
     demo: 'https://guitar-display.vercel.app/',
-    modalContent: 'This is a React app built with ViteJS. The app displays my 3D glb model created in Blender and uses <a href="https://github.com/pmndrs/drei#scrollcontrols" target="_blank" rel="noreferrer">ScrollControls</a> and GSAP timeline to animate model and user interface. It also includes a subpage route where you can interact with the same model. You can move around with <a href="https://threejs.org/docs/#examples/en/controls/OrbitControls" target="_blank" rel="noreferrer">OrbitControls</a>, click on different parts with camera foucus with <a href="https://github.com/pmndrs/drei#bounds" target="_blank" rel="noreferrer">Bounds</a>, display mesh name on hover near the cursor and change mesh material color. I learned a lot at about react-three-fiber at Wawa Sensei <a href="https://www.youtube.com/@WawaSensei" target="_blank" rel="noreferrer">YouTube channel</a> and <a href="https://github.com/pmndrs/drei" target="_blank" rel="noreferrer">react-three/drei</a> doucumentation.',
+    modalContent: 'This is a React app built with ViteJS. The app displays my 3D glb model created in Blender whitch uses <a href="https://github.com/pmndrs/drei#scrollcontrols" target="_blank" rel="noreferrer">ScrollControls</a> and GSAP timeline to animate model and user interface. It also includes a subpage route where you can interact with the same model. You can move around with <a href="https://threejs.org/docs/#examples/en/controls/OrbitControls" target="_blank" rel="noreferrer">OrbitControls</a>, click on different parts with camera foucus with <a href="https://github.com/pmndrs/drei#bounds" target="_blank" rel="noreferrer">Bounds</a>, display mesh name on hover near the cursor and change mesh material color.<p> I learned a lot at about react-three-fiber at Wawa Sensei <a href="https://www.youtube.com/@WawaSensei" target="_blank" rel="noreferrer">YouTube channel</a> and <a href="https://github.com/pmndrs/drei" target="_blank" rel="noreferrer">react-three/drei</a> doucumentation.</p>',
     modalImage: guit,
     technologies: '<li>Vite</li><li>SCSS</li><li>React</li><li>Three.js</li><li>React Three Fiber</li><li>GSAP</li><li>React Router</li>'
   }
